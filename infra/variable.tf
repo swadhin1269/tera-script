626c64bfdf06c8cd2ea9af6127e096769c237b9a
swadhin1269 <profile.swadhin@gmail.com>
2025-10-30 12:31:47 +0530

    structure changed

---
+++ infra/variable.tf
@@ -0,0 +1,13 @@
+##############
+# variables
+##############
+
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "buck2910251037"
+}
