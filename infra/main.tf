413aec66fe6d7cbc692c75caeb4fc727437b1bb5
swadhin1269 <profile.swadhin@gmail.com>
2025-10-31 12:15:50 +0530

    ec2-for linux practice

--- infra/main.tf
+++ infra/main.tf
@@ -16,10 +16,23 @@ provider "aws" {
 # OUTPUTS
 ###########################
 
-output "lambda_arn" {
-  value = aws_lambda_function.processor.arn
+# output "lambda_arn" {
+#   value = aws_lambda_function.processor.arn
+# }
+
+# output "s3_bucket_name" {
+#   value = aws_s3_bucket.process_bucket.bucket
+# }
+
+output "instance_id" {
+  value = aws_instance.web.id
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.process_bucket.bucket
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
+
+output "public_dns" {
+  value = aws_instance.web.public_dns
+}
+
